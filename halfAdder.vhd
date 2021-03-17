f348146bdf03ecdbc3af7833826b4ae29204f402
Eduardo Freitas <edufreitas.pb@gmail.com>
2021-03-17 15:02:11 -0300

    Update halfAdder.vhd

--- halfAdder.vhd
+++ halfAdder.vhd
@@ -5,7 +5,7 @@ ENTITY halfAdder IS
 	PORT ( 	
     	AB: IN BIT_VECTOR(1 DOWNTO 0);		-- A(1) B(0)
       	S_COUT: OUT BIT_VECTOR(1 DOWNTO 0);	-- S(1) COUT(0)
-END ha;
+END halfAdder;
 
 ARCHITECTURE HARDWARE OF halfAdder IS
 
