5067f152b2206c6b335750232b0a0e15d1c2d0c7
Eduardo Freitas <edufreitas.pb@gmail.com>
2021-03-17 14:27:51 -0300

    full adder
    A(2) B(1) CIN(0)
    S(1) COUT(0)

---
+++ fullAdder.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+ENTITY fullAdder IS
+	PORT(
+    	AB_CIN: IN BIT_VECTOR (2 DOWNTO 0);       -- A(2) B(1) CIN(0)
+        S_COUT: OUT BIT_VECTOR (1 DOWNTO 0));   -- S(1) COUT(0)
+END fullAdder;
+
+ARCHITECTURE HARDWARE OF fullAdder IS
+
+    BEGIN
+    
+    S_COUT(1) <= A XOR B XOR AB_CIN(0);
+	  S_COUT(0) <= (A AND B) OR (AB_CIN(0) AND (A XOR B));
+	
+END HARDWARE;
